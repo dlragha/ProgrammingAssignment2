699c55b25cc49e604bc0980e7954094e2cd6da4c
Deepti Ragha <deepti.ragha@gmail.com>
2015-07-26 16:02:45 -0400

    Programming Assignment 2 with correct comments
    Programming Assignment 2 with correct comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,8 @@
 ## Caching the Inverse of a Matrix
 
 ## This function creates a special "matrix" object that can cache its inverse.
-## It returns a list containing a function to:
-##
-## set the value of the matrix
-## get the value of the mtarix
-## set the inverse of the matrix
-## get the inverse of the matrix
-##
-##
-#############################################################################
-## Example used for testing: (Example credits : Paolo Di Lorenzo, from the course discussion page)
-##
-## > mat <- matrix(c(1,0,5,2,1,6,3,4,0),nrow = 3,ncol = 3)
-## > print(mat)
-## [,1] [,2] [,3]
-## [1,]    1    2    3
-## [2,]    0    1    4
-## [3,]    5    6    0
-## > matrixx <- makeCacheMatrix(mat)
-## > matrixx$get()
-## [,1] [,2] [,3]
-## [1,]    1    2    3
-## [2,]    0    1    4
-## [3,]    5    6    0
-## > matrixx$getinverse()
-## NULL
-## > cacheSolve(matrixx)
-## [,1] [,2] [,3]
-## [1,]  -24   18    5
-## [2,]   20  -15   -4
-## [3,]   -5    4    1
-## > matrixx$getinverse()
-## [,1] [,2] [,3]
-## [1,]  -24   18    5
-## [2,]   20  -15   -4
-## [3,]   -5    4    1
-## > class(matrixx$get())
-## [1] "matrix"
-## > class(matrixx$getinverse())
-## [1] "matrix"
-##
-#############################################################################
+## Input:  A matrix
+## Output: It returns a list containing a function to - set the value of the matrix, get the value of the mtarix, set the inverse of the matrix, get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inverseM <- NULL
